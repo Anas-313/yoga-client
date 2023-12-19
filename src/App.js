836872778294f268badb2106e36ad84811844e6d
Anas-313 <manaspatrawala313@gmail.com>
2023-12-19 23:17:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form'
+import Payment from './Components/Payment';
+import Header from './Components/Header';
+import Completion from './Components/Completion'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Form></Form>
+      <Payment></Payment>
+      <Completion></Completion>
     </div>
   );
 }
